8589ce7958b23db08b8e37a4423eb135dee6d966
xy <x944314910@gmail.com>
2026-07-08 04:53:43 +0800

    first commit

---
+++ src/main/java/com/example/xyproject/Controller/SqlController.java
@@ -0,0 +1,15 @@
+package com.example.xyproject.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SqlController {
+
+    @GetMapping("/test")
+    public String sql {
+
+        return "test";
+    }
+
+}
---
+++ src/test/java/com/example/xyproject/XyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
